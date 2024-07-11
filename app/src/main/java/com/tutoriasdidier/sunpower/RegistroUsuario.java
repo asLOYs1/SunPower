43f98acaeb6c1760e89fb74c7a877b2af06b791e
asLOYs1 <spachon9@gmail.com>
2024-07-11 00:51:25 -0500

    Initial commit

---
+++ app/src/main/java/com/tutoriasdidier/sunpower/RegistroUsuario.java
@@ -0,0 +1,19 @@
+package com.tutoriasdidier.sunpower;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegistroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario);
+
+    }
+}
